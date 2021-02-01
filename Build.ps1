64adc1898c376aea2eaab5e83784f077851a739e
Thomas Fuchs <tfuchs@phoenixcontact.com>
2021-02-01 11:06:15 +0100

    Prepare pipeline and packages

---
+++ Build.ps1
@@ -0,0 +1,39 @@
+﻿param (
+    [switch]$SetAssemblyVersion,
+    [switch]$Build,
+
+    [switch]$UnitTests,
+
+    [switch]$CoverReport,
+
+    [switch]$Pack,
+    [switch]$Publish
+)
+
+# Load Toolkit
+. ".build\BuildToolkit.ps1"
+
+# Initialize Toolkit
+Invoke-Initialize -Version (Get-Content "VERSION");
+
+if ($Build) {
+    Invoke-Build ".\MoryxFactory.sln"
+}
+
+if ($UnitTests) {
+    Invoke-CoverTests -SearchFilter "*.Tests.csproj"
+}
+
+if ($CoverReport) {
+    Invoke-CoverReport
+}
+
+if ($Pack) {
+    Invoke-PackAll
+}
+
+if ($Publish) {
+    Invoke-Publish
+}
+
+Write-Host "Success!"
