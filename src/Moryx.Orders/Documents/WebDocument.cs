8828fe9808d3507d4dd34adefd76cbba1c78bb81
Marcel Vielhaus <mvielhaus@phoenixcontact.com>
2022-11-09 14:58:23 +0100

    Add net6 Target and remove obsolete

--- src/Moryx.Orders/Documents/WebDocument.cs
+++ src/Moryx.Orders/Documents/WebDocument.cs
@@ -1,9 +1,10 @@
-﻿// Copyright (c) 2021, Phoenix Contact GmbH & Co. KG
+﻿// Copyright (c) 2022, Phoenix Contact GmbH & Co. KG
 // Licensed under the Apache License, Version 2.0
 
 using System.IO;
-using System.Net;
+using System.Net.Http;
 using System.Runtime.Serialization;
+using System.Threading.Tasks;
 
 namespace Moryx.Orders.Documents
 {
@@ -18,6 +19,8 @@ namespace Moryx.Orders.Documents
         /// </summary>
         [DataMember]
         public string Url { get; set; }
+
+        private static readonly HttpClient _client = new();
 
         /// <summary>
         /// Default constructor
@@ -37,13 +40,15 @@ namespace Moryx.Orders.Documents
         /// <inheritdoc />
         public override Stream GetStream()
         {
-            var request = WebRequest.Create(Url);
-            var response = request.GetResponse();
+            var getTask = Task.Run(() => _client.GetAsync(Url));
+            getTask.Wait();
+            var response = getTask.Result;
+            
+            var readTask = Task.Run(() => response.Content.ReadAsStreamAsync());
+            readTask.Wait();
 
-            ContentType = response.ContentType;
-            var dataStream = response.GetResponseStream();
-
-            return dataStream;
+            ContentType = response.Content.Headers.ContentType.MediaType;
+            return readTask.Result;
         }
     }
 }
