1c0f66aeba311bb1aed9f0da85e25c3340d8ee84
Thomas Fuchs <tfuchs@phoenixcontact.com>
2023-02-23 15:05:09 +0100

    Prepare new API for user input for instructions

--- src/Moryx.ControlSystem/VisualInstructions/EnumInstructionResult.cs
+++ src/Moryx.ControlSystem/VisualInstructions/EnumInstructionResult.cs
@@ -10,23 +10,38 @@ namespace Moryx.ControlSystem.VisualInstructions
     /// <summary>
     /// Represents an <see cref="IInstructionResults"/> which will handle enums to generate results and convert them back
     /// </summary>
-    public class EnumInstructionResult : IInstructionResults
+    public class EnumInstructionResult : IInstructionInputResults
     {
-        private readonly Action<int> _callback;
+        private readonly Action<int, object> _callback;
         private readonly Dictionary<string, int> _valueMap = new Dictionary<string, int>();
 
         /// <inheritdoc />
         public string[] Results => _valueMap.Keys.ToArray();
 
+        /// <inheritdoc />
+        public object Input { get; private set; }
+
+        /// <summary>
+        /// Creates a new instance of <see cref="EnumInstructionResult"/>
+        /// </summary>
+        [Obsolete("Use constructor with input object instead")]
+        public EnumInstructionResult(Type resultEnum, Action<int> callback, params string[] exceptions)
+            : this(resultEnum, null, (result, input) => callback(result), exceptions)
+        {
+        }
+
         /// <summary>
         /// Creates a new instance of <see cref="EnumInstructionResult"/>
         /// </summary>
         /// <param name="resultEnum">Enum type which will be used to create instruction results</param>
+        /// <param name="inputs">Input object</param>
         /// <param name="callback">Callback with enum result value of the executed instruction</param>
         /// <param name="exceptions">Excepted enum value names. Will be ignored for result</param>
-        public EnumInstructionResult(Type resultEnum, Action<int> callback, params string[] exceptions)
+        public EnumInstructionResult(Type resultEnum, object inputs, Action<int, object> callback, params string[] exceptions)
         {
             _callback = callback;
+
+            Input = inputs;
 
             var allHidden = true;
             var allValues = new Dictionary<string, int>();
@@ -58,11 +73,20 @@ namespace Moryx.ControlSystem.VisualInstructions
         /// Invokes the callback with the given string result
         /// Will parse the string to the enum value
         /// </summary>
-        /// <param name="result"></param>
         public virtual void Invoke(string result)
         {
             var enumValue = _valueMap[result];
-            _callback(enumValue);
+            _callback(enumValue, null);
+        }
+
+        /// <summary>
+        /// Invokes the callback with the given string result
+        /// Will parse the string to the enum value
+        /// </summary>
+        public void Invoke(string result, object input)
+        {
+            var enumValue = _valueMap[result];
+            _callback(enumValue, input);
         }
     }
 }
