6f7b376f183961efccd9e0dc9053d63280aceef5
psteib <psteib@phoenixcontact.com>
2025-04-28 13:21:50 +0200

    Localization of MORYX.Factory

--- src/Moryx.Factory/ManufacturingFactory.cs
+++ src/Moryx.Factory/ManufacturingFactory.cs
@@ -1,17 +1,22 @@
-﻿// Copyright (c) 2023, Phoenix Contact GmbH & Co. KG
+// Copyright (c) 2023, Phoenix Contact GmbH & Co. KG
 // Licensed under the Apache License, Version 2.0
 
 using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Runtime.Serialization;
 using Moryx.AbstractionLayer.Resources;
+using Moryx.Factory.Localizations;
 
 using Moryx.Serialization;
 
 namespace Moryx.Factory
 {
+    [ResourceRegistration]
+    [Display(Name = nameof(Strings.MANUFACTURING_FACTORY), Description = nameof(Strings.MANUFACTURING_FACTORY_DESCRIPTION), ResourceType = typeof(Localizations.Strings))]
     public class ManufacturingFactory : Resource, IManufacturingFactory
     {
-        [DataMember, EntrySerialize, DefaultValue("assets/Fabrik_Hintergrund.png"), Description("URL of the background picture of the Factory Monitor")]
+        [DataMember, EntrySerialize, DefaultValue("https://www.phoenixcontact.com/common/assets/images/signal-chain.svg")]
+        [Display(Name = nameof(Strings.BACKGROUND_URL), Description = nameof(Strings.BACKGROUND_URL_DESCRIPTION), ResourceType = typeof(Localizations.Strings))]
         public string BackgroundUrl { get; set; }
     }
 }
