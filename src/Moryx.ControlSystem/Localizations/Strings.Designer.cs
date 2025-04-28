e949c1fd492a4c4e232e8f00c98eff892d6819f1
psteib <psteib@phoenixcontact.com>
2025-04-28 13:37:11 +0200

    Renamed folder Properties to Localiszations for conistency

--- src/Moryx.ControlSystem/Localizations/Strings.Designer.cs
+++ src/Moryx.ControlSystem/Localizations/Strings.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Moryx.ControlSystem.Properties {
+namespace Moryx.ControlSystem.Localizations {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace Moryx.ControlSystem.Properties {
         public static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Moryx.ControlSystem.Properties.Strings", typeof(Strings).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Moryx.ControlSystem.Localizations.Strings", typeof(Strings).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
